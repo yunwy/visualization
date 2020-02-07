206b4d96916de6536959a4736fa67cc05d31935a
yunwy <58315588+yunwy@users.noreply.github.com>
2020-02-07 16:44:19 +0900

    Update 3) Van_der_pol_oscillator.py
    mu = 0 일때 진동자의 진폭이 증가하는 오류를 해결했습니다.

--- files/3) Van_der_pol_oscillator.py
+++ files/3) Van_der_pol_oscillator.py
@@ -1,42 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Slider
+from scipy.integrate import solve_ivp
 
 
-mu = 3.5
+mu = 2
 dt = 0.01
 t = np.arange(0, 50, dt)
 x0, y0 = 0, 0.1
 
 
-def vanderpol(mu, x0, y0):
-    x = [x0]
-    y = [y0]
-
-    for _ in t:
-        X, Y = x[-1], y[-1]
-        x.append(X + Y*dt)
-        y.append(Y + (mu*(1 - X**2)*Y - X)*dt)
-        
-    del x[-1], y[-1]
-
-    return (x, y)
+def vanderpol(t, z, mu):
+    x, y = z
+    return [y, mu*(1 - x**2)*y - x]
 
 
-(xt, yt) = vanderpol(mu, x0, y0)
+sol = solve_ivp(vanderpol, (t[0], t[-1]), (x0, y0),
+                args=[mu], method='RK45', dense_output=True)
+z = sol.sol(t)
 
 fig = plt.figure(figsize=(10, 5))
 plt.subplots_adjust(bottom=0.25)
 
-plt.subplot(1,2,1)
-line = plt.plot(t, xt)[0]
+ax1 = plt.subplot(1,2,1)
+ax1.set_ylim([-3, 3])
+line = plt.plot(t, z[0])[0]
 plt.xlabel('$t$')
 plt.ylabel('$x(t)$')
 plt.title('$\ddot{x}-\mu(1-x^2)\dot{x}+x=0$')
 plt.grid()
 
-plt.subplot(1,2,2)
-curve = plt.plot(xt, yt)[0]
+ax2 = plt.subplot(1,2,2)
+ax2.set_ylim([-7, 7])
+curve = plt.plot(z[0], z[1])[0]
 plt.xlabel('$x(t)$')
 plt.ylabel('$\dot{x}(t)$')
 plt.title('Phase plane')
@@ -47,7 +43,7 @@ ax_mu = plt.axes([0.13, 0.12, 0.74, 0.03])
 ax_x0 = plt.axes([0.13, 0.07, 0.74, 0.03])
 ax_y0 = plt.axes([0.13, 0.02, 0.74, 0.03])
 
-slider_mu = Slider(ax_mu, '$\mu$', 0, 20.0, valinit=mu)
+slider_mu = Slider(ax_mu, '$\mu$', 0, 4, valinit=mu)
 slider_x0 = Slider(ax_x0, '$x_0$', -2, 2, valinit=x0)
 slider_y0 = Slider(ax_y0, '$\dot{x}_0$', -6, 6, valinit=y0)
 
@@ -57,14 +53,14 @@ def upd(val):
     val_x0 = slider_x0.val
     val_y0 = slider_y0.val
     
-    update = vanderpol(val_mu, val_x0, val_y0)
+    update = solve_ivp(vanderpol, (t[0], t[-1]), (val_x0, val_y0),
+                        args=[val_mu], method='RK45', dense_output=True)
+    z = update.sol(t)
     
-    line.set_ydata(update[0])
-    curve.set_xdata(update[0])
-    curve.set_ydata(update[1])
-
-    if max(update[1]) > 6:
-        ax.set_ylim([min(update[1]) - 0.5, max(update[1]) + 0.5])
+    line.set_ydata(z[0])
+    curve.set_xdata(z[0])
+    curve.set_ydata(z[1])
+    
 
 slider_mu.on_changed(upd)
 slider_x0.on_changed(upd)
